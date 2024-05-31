980e699a6c72308b66acb431b6f5047ea0c4605d
brendan <brendangooch@gmail.com>
2024-05-31 16:58:18 +0100

    forgot to export files in index.ts barrel file... now added.

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@
  * a simple barrel file for the library
  */
 
+export { pointInCircle } from './point-in-circle.js';
+export { circleInCircle } from './circle-in-circle.js';
+export { pointInRectangle } from './point-in-rectangle.js';
+export { rectangleInRectangle } from './rectangle-in-rectangle.js';
+export { pointInRotatedRectangle } from './point-in-rotated-rectangle.js';
